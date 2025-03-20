17870a936956bb5b26152dfeed6cd6ac012bb1c3
Sheviakov Denis <saivrem@gmail.com>
2025-03-20 10:56:23 +0200

    Adjust __init__.py

---
+++ disk_utils/__init__.py
@@ -0,0 +1,4 @@
+from .file_walker import FileWalker
+from .consumer import FileConsumer, FileLister, FileMover, DotFileRemover
+
+__all__ = ['FileConsumer', 'DotFileRemover', 'FileMover', 'FileLister', 'FileWalker']
---
+++ disk_utils/consumer/__init__.py
@@ -0,0 +1,4 @@
+from .file_consumer import FileConsumer
+from .dotfile_remover import DotFileRemover
+from .file_mover import FileMover
+from .file_lister import FileLister
